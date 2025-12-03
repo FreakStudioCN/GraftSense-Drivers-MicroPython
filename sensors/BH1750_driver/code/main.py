abc36edbd43a83f261d648e308b144171d46b492
DESKTOP-BH39479\Administrator <1069653183@qq.com>
2025-12-03 18:24:31 +0800

    feat(G:\GraftSense-Drivers-MicroPython#\storage\sdcard_driver): 增加了SD卡驱动代码
    增加了 SD 卡相关驱动代码；

--- sensors/BH1750_driver/code/main.py
+++ sensors/BH1750_driver/code/main.py
@@ -23,8 +23,10 @@ BH_addr = None
 
 time.sleep(3)
 print("FreakStudio: test Light Intensity Sensor now")
+
 # Create I2C bus (adjust pins according to your board)
 i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=100000)
+
 # 开始扫描I2C总线上的设备，返回从机地址的列表
 devices_list: list[int] = i2c.scan()
 print('START I2C SCANNER')
