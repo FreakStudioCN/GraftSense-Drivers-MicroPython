a0ad4d0359aeed767581b1805b3e592a65be72a5
hogeiha <1098875044@QQ.COM>
2025-12-25 15:46:39 +0800

    fix(input/ps2_joystick_driver/code/main.py): 修改注释

--- input/ps2_joystick_driver/code/main.py
+++ input/ps2_joystick_driver/code/main.py
@@ -39,6 +39,6 @@ try:
         print("Joystick values: X = {:.2f}, Y = {:.2f}, Switch = {}".format(x_val, y_val, sw_val))
         time.sleep(0.2)
 except KeyboardInterrupt:
-    print("\nData collection completed")
+    print("Data collection completed")
 finally:
     joystick.stop()
