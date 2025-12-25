1ff2a0456e0c4d4af3bfc8f8d67245c88024011a
hogeiha <1098875044@qq.com>
2025-12-25 12:09:22 +0800

    docs(signal_generation/mcp4725_driver/code/main.py): 修改注释

--- signal_generation/mcp4725_driver/code/main.py
+++ signal_generation/mcp4725_driver/code/main.py
@@ -77,7 +77,7 @@ time.sleep(3)
 # 打印调试信息
 print("FreakStudio : Using DAC to generate sine wave")
 
-# 创建硬件I2C的实例，使用I2C1外设，时钟频率为400KHz，SDA引脚为2，SCL引脚为3
+# 创建硬件I2C的实例，使用I2C1外设，时钟频率为400KHz，SDA引脚为4，SCL引脚为5
 i2c = I2C(id=0, sda=Pin(4), scl=Pin(5), freq=400000)
 
 # 开始扫描I2C总线上的设备，返回从机地址的列表
