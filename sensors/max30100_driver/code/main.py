5cb9a3a42339023a8c2605a365b71eb4d66b37c4
hogeiha <1098875044@QQ.COM>
2026-02-03 11:59:04 +0800

    feat(sensors/max30102_driver/code): ir,red,血氧计算输出

--- sensors/max30100_driver/code/main.py
+++ sensors/max30100_driver/code/main.py
@@ -1,20 +1,47 @@
+# Python env   : MicroPython v1.27.0
+# -*- coding: utf-8 -*-
+# @Time    : 2026/02/03 11:00
+# @Author  : hogeiha
+# @File    : main.py
+# @Description : MAX30100 ir+red+血氧读取计算示例
+
+# ======================================== 导入相关模块 =========================================
+
 from machine import I2C, Pin
 import max30100
 import time
 
-red = None
-ir = None
 
+# ======================================== 全局变量 ============================================
 
-i2cMAX30100=I2C(0, scl=Pin((5)), sda=Pin((4)))
-max30100Sensor = max30100.MAX30100(i2c = i2cMAX30100)
-max30100Sensor.enable_spo2()
+# ======================================== 功能函数 ============================================
+
+# ======================================== 自定义类 ============================================
+
+# ======================================== 初始化配置 ==========================================
+
+# 上电延时3s
+time.sleep(3)
+# 打印调试消息
+print("FreakStudio: Use the MAX30100 to read values and calculate blood oxygen levels..")
+
+# I2C：SDA=GP4，SCL=GP5
+i2cMAX30100 = I2C(0, scl=Pin((5)), sda=Pin((4)))
+# 初始化传感器实例
+sensor = max30100.MAX30100(i2c = i2cMAX30100)
+sensor.enable_spo2()
+
+# ========================================  主程序  ===========================================
 while True:
-  max30100Sensor.read_sensor()
-  red = max30100Sensor.red
+  # 读取传感器数据
+  sensor.read_sensor()
+  red = sensor.red
+  ir = sensor.ir
 
-  ir = max30100Sensor.ir
-
-  print(f"red{red}")
+  if ir:
+    har = 100 - 25 * (red / ir)
+    print(f"har:{har}")
+  print(f"red:{red}")
   print(f"ir:{ir}")
+  
   time.sleep_ms(200)
